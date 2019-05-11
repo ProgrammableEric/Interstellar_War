ba31fc1f69db2f2a72faa9cf60e90276cce31353
Wenjuan Liao <u6469860@anu.edu.au>
2019-05-11 21:05:09 +1000

    Add a new class Star!

---
+++ app/src/main/java/com/example/interstellarwar/Star.java
@@ -0,0 +1,28 @@
+
+/**
+ * Planets that can be move straightly
+ */
+
+public class Star extends Planet {
+
+    private float speed; // the pixel number of each movement, go down for positive
+
+    public Star(Bitmap bitmap){
+        super(bitmap);
+    }
+
+    public void setSpeed(float speed){
+        this.speed = speed;
+    }
+
+    public float getSpeed(){
+        return speed;
+    }
+
+    @Override
+    protected void beforeDeploy() {
+    }
+
+    protected void finishDeploy(){
+    }
+}
