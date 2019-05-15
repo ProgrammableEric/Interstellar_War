99d3374dfa9158ee3ef5c03bb75d9b3b226329b8
Wenjuan Liao <u6469860@anu.edu.au>
2019-05-15 23:21:21 +1000

    Update class Planet!

--- app/src/main/java/com/example/interstellarwar/NewStar.java
+++ app/src/main/java/com/example/interstellarwar/NewStar.java
@@ -4,6 +4,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import java.util.List;
+
 public class NewStar extends Star {
     private int resistance = 1;//how the planet is resistant
     private int grade = 0;// how many grades can get after destroying one planet
@@ -30,11 +32,41 @@ public class NewStar extends Star {
 
     @Override
     protected void finishDeploy(Canvas canvas, Paint paint, GameView gameView){
-        //super.finishDeploy(canvas, paint, gameView);
+        super.afterDraw(canvas, paint, gameView);
+
+        // check if was shotted after deploying
+        if(!isDestroyed()){
+            // check if was shotted after star was deployed
+            List<Laser> laser = gameView.getAliveBullets();
+            for(Bullet bullet : bullets){
+                // check if star hit laser
+                Point p = getCollidePointWithOther(laser);
+                if(p != null){
+                    // if has intersection, laser hit star
+                    laser.destroy();
+                    resistance--;
+                    if(resistance <= 0){
+                        // no resistance, implement bomb
+                        bomb(gameView);
+                        return;
+                    }
+                }
+            }
+        }
     }
 
 
     public void bomb(GameView gameView){
+        // create bombing effect
+        float centerX = getX() + getWidth() / 2;
+        float centerY = getY() + getHeight() / 2;
+        Bitmap bitmap = gameView.getExplosionBitmap();
+        Bombing bombing = new Bombing(bitmap);
+        bombing.centerTo(centerX, centerY);
+        gameView.addPlanet(bombing);
 
+        // after bombing, add grades to GameView and detroy star
+        gameView.changeScore(grade);
+        destroy();
     }
 }
