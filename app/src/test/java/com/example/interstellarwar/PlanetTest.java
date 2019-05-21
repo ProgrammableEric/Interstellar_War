379e9cbb324a9f4a3a97dd153d572c8f479e0bcb
Yuyuan Liang <u6319242@anu.edu.au>
2019-05-21 23:52:38 +1000

    Merge branch 'master' of /Users/leoewing/AndroidStudioProjects/assignapp2019s1 with conflicts.

--- app/src/test/java/com/example/interstellarwar/PlanetTest.java
+++ app/src/test/java/com/example/interstellarwar/PlanetTest.java
@@ -1,19 +1,20 @@
 package com.example.interstellarwar;
 
-import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 //import static android.support.test.InstrumentationRegistry.getContext;
 import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.view.View;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URL;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class PlanetTest extends Activity {
+
+public class PlanetTest {
 
     private int[] bitmapIds = {
             R.drawable.spaceship,
@@ -30,16 +31,18 @@ public class PlanetTest extends Activity {
     };
 
     @Test
-    public void TestPositioning() {
+    public void TestPositioning(){
+        URL url = PlanetTest.class.getResource("");
+        System.out.println(url);
 
-//        Context context = MainActivity.class;
-//
-//        int bmid_mercury = bitmapIds[4];
-//        Bitmap bitmap_mercury = (Bitmap) BitmapFactory.decodeResource(MainActivity.class, bmid_mercury);
-//        Planet mercury1 = new Planet(bitmap_mercury);
-//        assertEquals("Planet initial x position is not 0", mercury1.getX() == 0);
-//        assertEquals("Planet initial y position is not 0", mercury1.getY() == 0);
+
+        int bmid_mercury = bitmapIds[4];
+        Bitmap bitmap_mercury = BitmapFactory.decodeResource(url, bmid_mercury);
+        Planet mercury1 = new Planet(bitmap_mercury);
+        assertEquals("Planet initial x position is not 0", mercury1.getX() == 0);
+        assertEquals("Planet initial x position is not 0", mercury1.getY() == 0);
     }
+
 
 
 }
